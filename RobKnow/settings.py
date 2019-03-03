a72a482cd02c734f8088ab035cf78c2c3e3141b8
Leon <kinglizuan@gmail.com>
2019-03-03 16:59:21 +0800

    new setting

--- RobKnow/settings.py
+++ RobKnow/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'o^ufj2s&yhfficl@ynht+!zrt$2$qx9+@&27_yc_^%q-gr6xi*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['agile-citadel-65280.herokuapp.com']
 
